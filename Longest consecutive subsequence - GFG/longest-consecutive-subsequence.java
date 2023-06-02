08f2f579cb58b1b01f29e067a4320dd250692e97
Cheshta Pathak <88539026+cheshtapathak@users.noreply.github.com>
2023-06-02 12:38:50 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   Map<Integer,Integer> m=new HashMap<>();
+	   TreeMap<Integer, Integer> sort=new TreeMap<>();
+	   int res=0;
+	   int ans=Integer.MIN_VALUE;
+	   for(int i=0;i<N;i++){
+	       if(!m.containsKey(arr[i]))
+	       m.put(arr[i],1);
+	   }
+	   sort.putAll(m);
+	   int temp=0;
+	   for(Map.Entry<Integer, Integer>e:sort.entrySet()){
+	       if(e.getKey()==temp+1){
+	           res+=e.getValue();
+	       } else
+	       res=1;
+	       ans=Math.max(ans,res);
+	       temp=e.getKey();
+	   }
+	   return ans;
+	}
+}
