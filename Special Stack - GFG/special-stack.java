8238baf1f58dd16b19483eca3145855f61940462
Cheshta Pathak <88539026+cheshtapathak@users.noreply.github.com>
2023-06-06 01:14:08 +0530

    Added solution - LeetHub

---
+++ Special Stack - GFG/special-stack.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.Stack;
+class SpeStack{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			Stack<Integer> s=new Stack<>();
+			GfG g=new GfG();
+			while(!g.isEmpty(s)){
+			    g.pop(s);
+			}
+			while(!g.isFull(s,n)){
+				g.push(sc.nextInt(),s);
+			}
+		System.out.println(g.min(s));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function(s) below*/
+class GfG{
+	public void push(int a,Stack<Integer> s)
+	{
+	    //add code here.
+	    s.push(a);
+	}
+	public int pop(Stack<Integer> s)
+        {
+            //add code here.
+            return s.pop();
+	}
+	public int min(Stack<Integer> s)
+        {
+           //add code here.
+           int mine1=s.peek();
+           while(!s.isEmpty()){
+               if(s.peek()<mine1){
+                   mine1=s.peek();
+               }
+               s.pop();
+           }
+           return mine1;
+	}
+	public boolean isFull(Stack<Integer>s, int n)
+        {
+           //add code here.
+           return s.size()==n;
+	}
+	public boolean isEmpty(Stack<Integer>s)
+        {
+           //add code here.
+           return s.size()==0;
+	}
+}
